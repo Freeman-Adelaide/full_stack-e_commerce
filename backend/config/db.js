f97a711c72a50522a4193b7939d47c9eae6305c2
Freeman-Adelaide <freemanijale@gmail.com>
2024-08-09 19:00:00 +0100

    Changed my database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URI)
-        .then(() => console.log('DB connected'))
+    try{
+    await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+    })
+        console.log("DB connected")
+    }catch(error){
+        console.error("Error connect to MongoDB: ", error.message)
+        process.exit(1); // Exit the process with failure
+    }
 }
 
